b2c6742343f98fb268b3a1a4239079d95c03789e
gunal15 <gunalnandakumar@gmail.com>
2023-02-24 16:19:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const cors=require("cors")
+const express = require("express");
+const mongo=require("./shared/mongo");
+const app=express();
+const  repoRoutes=require("./Routes/repo.routes");
+const tagsRoutes=require("./Routes/tags.routes");
+
+(async()=>{
+try{
+    await mongo.connect()
+ app.use(cors())
+
+app.use(express.json())
+
+ app.use("/repo",repoRoutes)
+
+  app.use("/tags",tagsRoutes)
+
+  app.get('/', function(req, res, next) {
+    res.send('AWS Polymer Search Clone Server Running Sucessfully');
+  });
+
+const port=process.env.PORT
+
+app.listen(port ||3000,()=>{
+    console.log("server running in port ",port)
+})
+}
+catch (err)
+{
+    console.log("Error in connecting to DB",)
+}
+})();
