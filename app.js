64ca80ae92fa71059a3f24b81d5310b083edd824
subustark <subustark25@gmail.com>
2023-04-08 12:19:04 +0530

    change

--- app.js
+++ app.js
@@ -1,34 +1,32 @@
 require("dotenv").config();
-const cors=require("cors")
+const cors = require("cors");
 const express = require("express");
-const mongo=require("./shared/mongo");
-const app=express();
-const  repoRoutes=require("./Routes/repo.routes");
-const tagsRoutes=require("./Routes/tags.routes");
+const mongo = require("./shared/mongo");
+const app = express();
+const repoRoutes = require("./Routes/repo.routes");
+const tagsRoutes = require("./Routes/tags.routes");
 
-(async()=>{
-try{
-    await mongo.connect()
-   app.use(cors())
+(async () => {
+  try {
+    await mongo.connect();
+    app.use(cors());
 
-   app.use(express.json())
+    app.use(express.json());
 
-   app.use("/repo",repoRoutes)
+    app.use("/repo", repoRoutes);
 
-   app.use("/tags",tagsRoutes)
+    app.use("/tags", tagsRoutes);
 
-   app.get('/', function(req, res, next) {
-    res.send('Server running successfully. The API endpoints are /repo,/tags');
-  });
+    app.get("/", function (req, res, next) {
+      res.send("Server Runs Perfectly. The API endpoints are /repo,/tags");
+    });
 
-const port=process.env.PORT
+    const port = process.env.PORT;
 
-app.listen(port ||3000,()=>{
-    console.log("Port: ",port)
-})
-}
-catch (err)
-{
-    console.log("Error in connecting to DB",)
-}
+    app.listen(port || 3000, () => {
+      console.log("Port: ", port);
+    });
+  } catch (err) {
+    console.log("Error in connecting to DB");
+  }
 })();
